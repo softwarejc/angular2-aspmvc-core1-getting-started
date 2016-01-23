b8425fdc71e4a3aaeb721ca1c6cdf1fbdecb57d2
Juan Carlos Sanchez <jcarlos.sanchez.calle@gmail.com>
2016-01-23 17:26:04 +0100

    Getting started with aspnet core 1 and angular2.

---
+++ SampleProject-ASPCore1-Angular2/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace GettingStarted_ASPNetCore1_Angular2.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get(string name)
+        {
+            var result = $"Hello {name}, the server UTC time is: {DateTime.UtcNow}";
+            return new ObjectResult(result);
+        }
+    }
+}
