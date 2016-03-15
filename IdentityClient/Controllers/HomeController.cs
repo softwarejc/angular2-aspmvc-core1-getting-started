fb0bda324a3df152eb74f991af8b33a04be427a8
softwarejc <jcarlos.sanchez.calle@gmail.com>
2016-03-15 14:33:00 +0100

    Add IdentityServer demo Client UI

---
+++ IdentityClient/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace IdentityClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            ViewData["Message"] = "Your account page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
